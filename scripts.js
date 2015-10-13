81d8fec891de18c0996e60792a8a7b9f87617af8
Deanna O'Neal <deannaboneal@gmail.com>
2015-10-13 19:37:19 -0400

    aligned text and logo to center

---
+++ scripts.js
@@ -0,0 +1,20 @@
+//Problem: user when clicking on image goes to a dead end
+//Solution: create an overlay with the large image - lightbox
+
+//1 Capture the click event on a link to an image
+
+//$("#imageGallery a").click(function(event){
+  //event.preventDefault();
+  //var href = $(this).attr("href");
+  //console.log(href);
+  //1.1 Show the overlay
+  //1.2 Update overlay with the image linked in the link
+  //1.3 Get child's alt attribute and set caption
+
+//});
+
+//2 Add overlay
+  //2.1 An image
+  //2.2 A caption
+//3 When overlay is clicked
+  //3.1 Hide the overlay
